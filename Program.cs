9552a7a8b0c2c856cdd4ecb2705644388d197d2b
villjr <61257261+villjr@users.noreply.github.com>
2020-02-26 20:46:30 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_4._6_jorgevillarreal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] days = new string[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            string[] tasks = new string[7];
+
+            for (int i = 0; i < days.Length; i++)
+            {
+
+                Console.WriteLine("Type in a new Task for " + days[i]);
+                tasks[i] = days[i] + " : " + Console.ReadLine();
+
+            }
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(tasks[i]);
+            }
+        }
+    }
+}
